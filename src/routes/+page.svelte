fef30c16a7f83c132fe17b5699afbf3803550757
Tae Yeong Kim <53635845+TaetaeMVS@users.noreply.github.com>
2023-04-22 19:13:20 +1000

    import sveltekit skeleton

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<div id="wrapper">
+    <div id="bg"></div>
+    <div id="overlay"></div>
+    <div id="main">
+
+        <!-- Header -->
+            <header id="header">
+                <h1>Tae Yeong Kim</h1>
+                <p>Devops Engineering Intern @ NAB &nbsp;&bull;&nbsp; Finance/Computer Science @ UniMelb &nbsp;&bull;&nbsp; Aspiring full stack engineer</p>
+                <nav>
+                    <ul>
+                        <li><a href="https://www.linkedin.com/in/taeyeong-kim-616151198/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+                        <li><a href="https://github.com/TaetaeMVS" class="icon brands fa-github"><span class="label">Github</span></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCfr7CkcS7QREwmSxRCjDgqg" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+                        <li><a href="taeyeong2000@gmail.com" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+                    </ul>
+                </nav>
+            </header>
+
+        <!-- Footer -->
+            <footer id="footer">
+                <span class="copyright">&copy; Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
+            </footer>
+
+    </div>
+</div>
