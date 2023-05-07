d9bf4d17c04d45482b81d629cde3bc7aad838c04
Tae Yeong Kim <53635845+TaetaeMVS@users.noreply.github.com>
2023-05-07 17:19:53 +1000

    refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,23 @@
 <script>import "../app.css";</script>
-<div id="wrapper">
-    <div id="bg"></div>
-    <div id="overlay"></div>
-    <div id="main">
 
-        <!-- Header -->
-            <header id="header">
-                <h1>Tae Yeong Kim</h1>
-                <p>Intern Devops Engineer @ NAB &nbsp;&bull;&nbsp; Finance/Computer Science @ UniMelb &nbsp;&bull;&nbsp; Aspiring full stack engineer</p>
-                <nav>
-                    <ul>
-                        <li><a href="https://www.linkedin.com/in/taeyeong-kim-616151198/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
-                        <li><a href="https://github.com/TaetaeMVS" class="icon brands fa-github"><span class="label">Github</span></a></li>
-                        <li><a href="https://www.youtube.com/channel/UCfr7CkcS7QREwmSxRCjDgqg" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
-                        <li><a href="mailto:taeyeong2000@gmail.com" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
-                    </ul>
-                </nav>
-                <nav class="nav-square">
-                    <ul>
-                        <li><a href="/resume" class="fa-solid fa-file" data-text="Resume"><span class="icon"></span><span class="label">Resume</span></a></li>
-                        <li><a href="/projects" class="fa-solid fa-code" data-text="Projects"><span class="icon"></span><span class="label">Projects</span></a></li>
-                    </ul>
-                </nav>
-            </header>
 
-        <!-- Footer -->
-            <footer id="footer">
-                <span class="copyright">&copy; Design: <a href="http://html5up.net">HTML5 UP</a></span>
-            </footer>
+<!-- Header -->
+<header id="header">
+    <h1>Tae Yeong Kim</h1>
+    <p>Intern Devops Engineer @ NAB &nbsp;&bull;&nbsp; Finance/Computer Science @ UniMelb &nbsp;&bull;&nbsp; Aspiring full stack engineer</p>
+    <nav>
+        <ul>
+            <li><a href="https://www.linkedin.com/in/taeyeong-kim-616151198/" class="icon brands fa-linkedin"><span class="label">LinkedIn</span></a></li>
+            <li><a href="https://github.com/TaetaeMVS" class="icon brands fa-github"><span class="label">Github</span></a></li>
+            <li><a href="https://www.youtube.com/channel/UCfr7CkcS7QREwmSxRCjDgqg" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+            <li><a href="mailto:taeyeong2000@gmail.com" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+    </nav>
+    <nav class="nav-square">
+        <ul>
+            <li><a href="/resume" class="fa-solid fa-file" data-text="Resume"><span class="icon"></span><span class="label">Resume</span></a></li>
+            <li><a href="/projects" class="fa-solid fa-code" data-text="Projects"><span class="icon"></span><span class="label">Projects</span></a></li>
+        </ul>
+    </nav>
+</header>
 
-    </div>
-</div>
