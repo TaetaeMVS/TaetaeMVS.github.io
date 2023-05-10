8f6a553708a8ee9dac2dcec166e651616d4e349b
Tae Yeong Kim <53635845+TaetaeMVS@users.noreply.github.com>
2023-05-10 19:05:27 +1000

    update resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,5 +1,9 @@
 <script>import "./resume.css";</script>
 <header id="header">
     <h1>Resume</h1>
-    <p>&nbsp;&bull;&nbsp; Devops Engineering Intern at NAB (February 2022 - current)</p>
+    <h2 id="devops-header">Devops Engineering Intern at NAB (February 2022 - current)<h2>
+    <ul>
+        <li>&nbsp;&bull;&nbsp; Successfully built, deployed and maintained CI/CD pipelines through a custom Jenkins environment, significantly reducing the time taken
+        for developers to test and deploy code.</li>
+    </ul>
 </header>
