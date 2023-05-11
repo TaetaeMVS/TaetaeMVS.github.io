3d699f6a2ee67d9821b8549a385a60856bc471ae
Tae Yeong Kim <53635845+TaetaeMVS@users.noreply.github.com>
2023-05-11 18:36:09 +1000

    update resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -5,5 +5,6 @@
     <ul>
         <li>&nbsp;&bull;&nbsp; Successfully built, deployed and maintained CI/CD pipelines through a custom Jenkins environment, significantly reducing the time taken
         for developers to test and deploy code.</li>
+        <li>&nbsp;&bull;&nbsp; Wrote pipelines to build and deploy custom RHEL-based IBM DataPower docker images, including building the dockerfile, and provisioning all resources in AWS ECS.</li>
     </ul>
 </header>
